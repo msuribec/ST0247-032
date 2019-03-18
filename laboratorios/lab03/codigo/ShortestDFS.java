a1f29ae4f73a0ed73f73326784f8ef886183807c
msuribec <37965605+msuribec@users.noreply.github.com>
2019-03-17 19:37:17 -0500

    upload code

---
+++ laboratorios/lab03/codigo/ShortestDFS.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ShortestDFS {
+
+    public static int  Dfs (Digraph g, int src ,int end){
+        int [] dist = new int[g.size()];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src] =0;
+        Dfs(g,src,dist);
+        System.out.print(Arrays.toString(dist));
+        return dist[end];
+    }
+
+    private static  void Dfs (Digraph g, int src, int []dist){
+        ArrayList<Integer> adyacentes = g.getSuccessors(src);
+
+        for (Integer u: adyacentes){
+            int w =  g.getWeight(src,u)+ dist[src];
+            if (dist[u]> w){
+                dist[u]= w;
+                Dfs(g, u, dist);
+            }
+        }
+
+    }
+
+
+}
