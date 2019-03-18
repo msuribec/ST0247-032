e27a7815382d0a429581ec6a06c80b1591e06a72
msuribec <37965605+msuribec@users.noreply.github.com>
2019-03-17 19:43:59 -0500

    Add files via upload

---
+++ laboratorios/lab03/codigo/Tests.java
@@ -0,0 +1,71 @@
+
+/**
+ * Prueba la implementacion de los metodos en la clase ShortestDFS
+ *
+ * @author María Sofía Uribe
+ */
+public class Tests {
+
+    public static void main(String[] args) {
+
+        System.out.println("ShortestPathDFS-> " + convert(spathDFS()));
+
+    }
+
+    static boolean spathDFS() {
+//        DigraphAL g = new DigraphAL(8);
+//        g.addArc(0,1,20);
+//        g.addArc(0,3,80);
+//        g.addArc(0,6,90);
+//
+//        g.addArc(1,5,10);
+//
+//        g.addArc(2,7,20);
+//        g.addArc(2,5,50);
+//        g.addArc(2,3,10);
+//
+//        g.addArc(3,2,10);
+//        g.addArc(3,6,20);
+//
+//        g.addArc(4,1,50);
+//        g.addArc(4,6,30);
+//
+//        g.addArc(5,2,10);
+//        g.addArc(5,3,40);
+//
+//        g.addArc(6,0,20);
+//
+//        if (ShortestDFS.Dfs(g,0,0)!=0)return false;
+//        if (ShortestDFS.Dfs(g,0,1)!=20)return false;
+//        if (ShortestDFS.Dfs(g,0,2)!=40)return false;
+//        if (ShortestDFS.Dfs(g,0,3)!=50)return false;
+//        if (ShortestDFS.Dfs(g,0,4)!=Integer.MAX_VALUE) return false;
+//        if (ShortestDFS.Dfs(g,0,5)!=30)return false;
+//        if (ShortestDFS.Dfs(g,0,6)!=70)return false;
+//        if (ShortestDFS.Dfs(g,0,7)!=60)return false;
+//
+
+        DigraphAL g1 = new DigraphAL(5);
+        g1.addArc(1,2,2);
+        g1.addArc(1,4,5);
+        g1.addArc(1,2,4);
+
+        g1.addArc(0,3,1);
+
+        g1.addArc(3,2,3);
+        g1.addArc(2,4,1);
+
+
+       System.out.print(ShortestDFS.Dfs(g1,0,3));
+
+        return true;
+
+    }
+
+
+
+    static String convert(boolean b) {
+        return b ? "correcta" : "incorrecta";
+    }
+
+}
