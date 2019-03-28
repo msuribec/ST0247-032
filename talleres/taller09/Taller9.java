2ee2901c4e1cea663b0a849f53b8253e289a9188
msuribec <37965605+msuribec@users.noreply.github.com>
2019-03-28 10:59:51 -0500

    update taller9

--- talleres/taller09/Taller9.java
+++ talleres/taller09/Taller9.java
@@ -20,27 +20,18 @@ class Taller9{
         
         for (int i=0; i<=m; i++) { 
             for (int j=0; j<=n; j++) { 
-                 
                 if (i==0) d[0][j] = j;  
-      
-                
                 else if (j==0) d[i][j] = i; 
-       
-                
                 else if (str1.charAt(i-1) == str2.charAt(j-1)) 
                     d[i][j] = d[i-1][j-1]; 
-       
-                
+
                 else{
                     d[i][j] = Math.min(d[i][j-1],  
                                        d[i-1][j]);
-                    d[i][j]  = 1 + Math.min (d[i-1][j-1],d[i][j]);             
-                                      
-                }
-                    
+                    d[i][j]  = 1 + Math.min (d[i-1][j-1],d[i][j]);                                  
+                }     
             } 
-        } 
-   
+        }
         return d[m][n]; 
     } 
   
